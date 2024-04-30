dc2bd35b7351c51a1b57f4eda329923484310bd6
ar1friz1 <151876294+ar1friz1@users.noreply.github.com>
2024-04-30 23:13:53 +0600

    Update Customer.java

--- Banking system/src/Customer.java
+++ Banking system/src/Customer.java
@@ -4,27 +4,27 @@ public class Customer {
         private String lastName;
 
         public Customer() {
-        }
+        }                        // constructor without parameter
 
         public Customer(String firstName, String lastName) {
-            this.firstName = firstName;
-            this.lastName = lastName;
+            this.firstName = firstName;   
+            this.lastName = lastName;                // constructor with parameter 
         }
 
         public String getFirstName() {
-            return firstName;
+            return firstName;                //getter method to access private variables 
         }
 
         public void setFirstName(String FName) {
-            this.firstName = FName;
+            this.firstName = FName;                //setter method to set private variables 
         }
 
         public String getLastName() {
-            return lastName;
+            return lastName;                //getter method to access private variables 
         }
 
-        public void setLastName(String LName) {
-            this.lastName = lastName;
+        public void setLastName(String LName) {        
+            this.lastName = lastName;                //setter method to set private variables 
         }
     }
 
