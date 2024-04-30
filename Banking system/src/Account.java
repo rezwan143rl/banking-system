15d30c8eb65ae12b7ce7ea78aa3becec954a4cb1
ar1friz1 <151876294+ar1friz1@users.noreply.github.com>
2024-04-30 23:18:10 +0600

    Update Account.java

--- Banking system/src/Account.java
+++ Banking system/src/Account.java
@@ -1,28 +1,31 @@
+// used inheritance 
+
 public class Account extends Customer{
+       // used encapsulation
     private int balance;
-    private int accountNumber;
+    private int accountNumber; 
 
-
+    // constructor 
     public Account(String FName, String LName, int accBal, int accNum) {
-        super(FName, LName);
-        this.balance = accBal;
+        super(FName, LName); // to access variable of the parent class 
+        this.balance = accBal;       
         this.accountNumber = accNum;
     }
 
     public int getBalance() {
-        return balance;
+        return balance;        //getter method to access private variables 
     }
 
     public int getAccountNumber() {
-        return accountNumber;
+        return accountNumber;    //getter method to access private variables 
     }
 
     public void setBalance(int balance) {
-        this.balance = balance;
+        this.balance = balance;    //setter method to access private variables 
     }
 
 
-
+    // method to deposit money
     public void deposit(int amount)
     {
      int temp;
@@ -31,6 +34,7 @@ public class Account extends Customer{
      setBalance(temp);
         System.out.println("Amount = "+amount+" is added successfully to the account.\n new balance is "+getBalance());
     }
+    // method to withdraw money
     public void withdraw(int amount)
     {
         int temp;
